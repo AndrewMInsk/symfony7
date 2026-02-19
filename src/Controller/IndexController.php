f50f3aca7e43118dc80a5af13e2015662e988d2d
info <info@a-site.by>
2026-02-19 21:18:46 +0300

    init

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,9 +5,9 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class LuckyController
+class IndexController
 {
-    #[Route('/lucky/number')]
+    #[Route('/index/number')]
     public function number(): Response
     {
         $number = random_int(0, 100);
